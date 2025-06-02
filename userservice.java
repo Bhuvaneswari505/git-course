1b82b125f23e60be7777d07ef26a3716b19f14d4
Ch.Bhuvaneswari <cherukumillibhuvaneswari@gmail.com>
2025-06-02 20:53:36 +0530

    creating new branch

--- userservice.java
+++ userservice.java
@@ -2,5 +2,6 @@ public class userservice{
     public static void main(String[] args){
         System.out.println("Here this file we are adding from local repo to remote repo that is in  github.");
         System.out.println("Github tagging ");
+        System.out.println("switching to new branch");
     }
-}
+} 
