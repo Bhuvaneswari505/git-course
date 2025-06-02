c58e6eca7794e2d78571d1135331779f7e2a2f44
Ch.Bhuvaneswari <cherukumillibhuvaneswari@gmail.com>
2025-06-02 11:49:24 +0530

    user service

---
+++ userservice.java
@@ -0,0 +1,5 @@
+public class userservice{
+    public static void main(String[] args){
+        System.out.println("Here this file we are adding from local repo to remote repo that is in  github.");
+    }
+}
