76833b71d21c789d926ff9fea1ff1a49921a8c87
Bhuvaneswari <123800444+Bhuvaneswari505@users.noreply.github.com>
2025-06-03 16:50:08 +0530

    Update adminservice.java

--- adminservice.java
+++ adminservice.java
@@ -3,7 +3,7 @@
 public class adminservice {
     public static void main(String[] args){
         System.out.println("pushing branch to remote repo that is github");
-        System.out.println("trying git rebase huh!!!");
+        System.out.println("trying the git rebase huh!!!");
         System.out.println("doing another commit haha");
    System.out.println("change the code");
     }
