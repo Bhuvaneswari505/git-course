b363e829acc95fb720779cd48756ca1fb9671788
Ch.Bhuvaneswari <cherukumillibhuvaneswari@gmail.com>
2025-06-03 11:47:41 +0530

    doing another commit hmmm..

--- adminservice.java
+++ adminservice.java
@@ -3,7 +3,8 @@
 public class adminservice {
     public static void main(String[] args){
         System.out.println("pushing branch to remote repo that is github");
-        System.out.println("trying git rbase huh");
+        System.out.println("trying git rebase huh");
+        System.out.println("doing another commit haha");
     }
     
 }
