c3466eb11f5213076868d2864067d40a85c2141c
Ch.Bhuvaneswari <cherukumillibhuvaneswari@gmail.com>
2025-06-03 11:41:13 +0530

    modified adminservice file

--- adminservice.java
+++ adminservice.java
@@ -3,6 +3,7 @@
 public class adminservice {
     public static void main(String[] args){
         System.out.println("pushing branch to remote repo that is github");
+        System.out.println("trying git rbase huh");
     }
     
 }
